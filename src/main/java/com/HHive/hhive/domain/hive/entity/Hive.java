8deaea0985d0d2a4566071fecf9db5af89ad047d
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2024-01-05 20:57:58 +0900

    feature: Hive 엔티티 구현

---
+++ src/main/java/com/HHive/hhive/domain/hive/entity/Hive.java
@@ -0,0 +1,49 @@
+package com.HHive.hhive.domain.hive.entity;
+
+import com.HHive.hhive.domain.category.entity.Category;
+import com.HHive.hhive.domain.user.entity.User;
+import com.HHive.hhive.global.auditing.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "hives")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hive extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long creater_id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String introduction;
+
+    @Column
+    @Builder.Default
+    private Boolean isDeleted = false;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
