81379ce6f980ddaa5c9580a52da27482ab0c68c7
Dongha <wkdehdgk159@gmail.com>
2024-01-05 18:23:39 +0900

    fix: 엔티티 구조 구축만 하려는 의도에 맞게 로직 삭제

--- src/main/java/com/HHive/hhive/domain/party/service/PartyService.java
+++ src/main/java/com/HHive/hhive/domain/party/service/PartyService.java
@@ -1,9 +1,5 @@
 package com.HHive.hhive.domain.party.service;
 
-import com.HHive.hhive.domain.hive.Hive;
-import com.HHive.hhive.domain.party.dto.PostPartyRequestDTO;
-import com.HHive.hhive.domain.party.entity.Party;
-import com.HHive.hhive.domain.party.repository.PartyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,21 +7,4 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PartyService {
 
-    private final PartyRepository partyRepository;
-
-    private final HiveRepository hiveRepository;
-
-    public String createParty(Long hiveId, PostPartyRequestDTO requestDTO) {
-
-        Hive hive = hiveRepository.findById(hiveId).orElseThrow(NOT_FOUND_HIVE_EXCEPTION);
-
-        Party party = Party.builder()
-                .content(requestDTO.getContent())
-                .hostId(requestDTO.getHostId())
-                .build();
-
-        partyRepository.save(party);
-
-        return "파티 생성 성공";
-    }
 }
