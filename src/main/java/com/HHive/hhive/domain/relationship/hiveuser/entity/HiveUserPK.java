738edddf72882f1b522228cdf362c8e4e8dc1f56
mixedtape <54096846+mixedtape@users.noreply.github.com>
2024-01-08 21:18:37 +0900

    feat: HiveUser 생성, Repository생성

---
+++ src/main/java/com/HHive/hhive/domain/relationship/hiveuser/entity/HiveUserPK.java
@@ -0,0 +1,24 @@
+package com.HHive.hhive.domain.relationship.hiveuser.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+public class HiveUserPK implements Serializable {
+
+    @Column(name="user_id")
+    private Long userId;
+
+    @Column(name="hive_id")
+    private Long hiveId;
+
+    @Builder
+    public HiveUserPK(Long userId, Long hiveId){
+        this.hiveId=hiveId;
+        this.userId=userId;
+    }
+}
