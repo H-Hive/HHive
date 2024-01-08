f146aa24670dd3bf2bfb6b09c675efb272c40f25
mixedtape <54096846+mixedtape@users.noreply.github.com>
2024-01-08 22:01:23 +0900

    feat:

--- src/main/java/com/HHive/hhive/domain/hive/entity/Hive.java
+++ src/main/java/com/HHive/hhive/domain/hive/entity/Hive.java
@@ -13,11 +13,13 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "hives")
 @Builder
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Hive extends BaseTimeEntity {
