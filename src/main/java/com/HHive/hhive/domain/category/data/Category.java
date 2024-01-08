082e0c5c8a83c517b92f161ad6a3af29d215a058
Dongha <wkdehdgk159@gmail.com>
2024-01-08 19:04:57 +0900

    Feature: 카테고리 조회 API 기능 구현

--- src/main/java/com/HHive/hhive/domain/category/data/Category.java
+++ src/main/java/com/HHive/hhive/domain/category/data/Category.java
@@ -1,7 +1,9 @@
-package com.HHive.hhive.global.common;
+package com.HHive.hhive.domain.category.data;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import lombok.Getter;
@@ -35,5 +37,28 @@ public enum Category {
 
     public Optional<Category> getParentCategory() {
         return Optional.ofNullable(parentCategory);
+    }
+
+    public static Map<String, List<String>> getAllCategories() {
+
+        Map<String, List<String>> categories = new HashMap<>();
+
+        for(Category category: Category.class.getEnumConstants()) {
+            if(category.getParentCategory().isEmpty()) continue;
+            if(category.getParentCategory().get().equals(Category.ROOT)) {
+                categories.put(category.getTitle(), new ArrayList<>());
+            }
+        }
+
+        for(Category category: Category.class.getEnumConstants()) {
+            for(String s: categories.keySet()) {
+                if(category.getParentCategory().isEmpty()) continue;
+                if(category.getParentCategory().get().getTitle().equals(s)) {
+                    categories.get(s).add(category.getTitle());
+                }
+            }
+        }
+
+        return categories;
     }
 }
