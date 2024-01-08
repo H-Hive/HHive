1be9d898d83fec9d9b17eafe4cc8b6fef93249b4
mixedtape <54096846+mixedtape@users.noreply.github.com>
2024-01-08 21:15:46 +0900

    update: partyUser 수정

--- src/main/java/com/HHive/hhive/domain/relationship/partyuser/entity/PartyUser.java
+++ src/main/java/com/HHive/hhive/domain/relationship/partyuser/entity/PartyUser.java
@@ -1,21 +1,43 @@
 package com.HHive.hhive.domain.relationship.partyuser.entity;
 
+import com.HHive.hhive.domain.party.entity.Party;
+
 import com.HHive.hhive.domain.relationship.partyuser.pk.PartyUserPK;
+import com.HHive.hhive.domain.user.entity.User;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
 @Entity
-@Builder
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
+@Getter
 public class PartyUser {
-
     @EmbeddedId
     private PartyUserPK partyUserPK;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    @MapsId("userId")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "party_id")
+    @MapsId("partyId")
+    private Party party;
+
+    @Builder
+    public PartyUser(User user , Party party){
+        this.user= user;
+        this.party=party;
+        this.partyUserPK=PartyUserPK.builder()
+                .userId(user.getId())
+                .partyId(party.getId())
+                .build();
+    }
 }
--- src/main/java/com/HHive/hhive/domain/relationship/partyuser/pk/PartyUserPK.java
+++ src/main/java/com/HHive/hhive/domain/relationship/partyuser/pk/PartyUserPK.java
@@ -1,32 +1,24 @@
 package com.HHive.hhive.domain.relationship.partyuser.pk;
 
-import com.HHive.hhive.domain.party.entity.Party;
-import com.HHive.hhive.domain.user.entity.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import java.io.Serializable;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@Builder
 @Embeddable
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EqualsAndHashCode
+@NoArgsConstructor
 public class PartyUserPK implements Serializable {
 
-    @JoinColumn(name = "party_id", nullable = false)
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private Party party;
+    @Column(name = "user_id")
+    private Long userId;
 
-    @JoinColumn(name = "user_id", nullable = false)
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private User user;
+    @Column(name = "party_id")
+    private Long partyId;
+
+    @Builder
+    public PartyUserPK(Long userId,Long partyId){
+        this.partyId=partyId;
+        this.userId=userId;
+    }
 }
