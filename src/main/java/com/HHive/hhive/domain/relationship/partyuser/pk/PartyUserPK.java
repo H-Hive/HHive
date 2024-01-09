78d6d10a368ceb159cdee5174dd87e51161f9707
de123456sdf <cdaeun95@gmail.com>
2024-01-09 17:32:39 +0900

    PartyUserPK import수정

--- src/main/java/com/HHive/hhive/domain/relationship/partyuser/pk/PartyUserPK.java
+++ src/main/java/com/HHive/hhive/domain/relationship/partyuser/pk/PartyUserPK.java
@@ -1,18 +1,14 @@
 package com.HHive.hhive.domain.relationship.partyuser.pk;
 
 import com.HHive.hhive.domain.party.entity.Party;
-import com.HHive.hhive.domain.user.User;
+import com.HHive.hhive.domain.user.entity.User;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.*;
+
 import java.io.Serializable;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
