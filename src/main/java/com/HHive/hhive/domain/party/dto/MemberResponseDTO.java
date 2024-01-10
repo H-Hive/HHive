81431d2e3ea3c264d554aa8bce48bfdaeca620fa
de123456sdf <cdaeun95@gmail.com>
2024-01-10 20:55:07 +0900

    MemberResponseDTO 수정

---
+++ src/main/java/com/HHive/hhive/domain/party/dto/MemberResponseDTO.java
@@ -0,0 +1,14 @@
+package com.HHive.hhive.domain.party.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberResponseDTO {
+    private String username;
+    private String email;
+}
