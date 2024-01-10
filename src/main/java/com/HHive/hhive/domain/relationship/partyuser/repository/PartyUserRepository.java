afe7fd04b05f8d0f502e6423e5f61245382a7361
de123456sdf <cdaeun95@gmail.com>
2024-01-10 20:59:47 +0900

    중복 검사 existsBy~ 추가

--- src/main/java/com/HHive/hhive/domain/relationship/partyuser/repository/PartyUserRepository.java
+++ src/main/java/com/HHive/hhive/domain/relationship/partyuser/repository/PartyUserRepository.java
@@ -2,11 +2,16 @@ package com.HHive.hhive.domain.relationship.partyuser.repository;
 
 import com.HHive.hhive.domain.relationship.partyuser.entity.PartyUser;
 import com.HHive.hhive.domain.relationship.partyuser.pk.PartyUserPK;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PartyUserRepository extends JpaRepository<PartyUser, PartyUserPK> {
     @Query("SELECT pu FROM PartyUser pu JOIN FETCH pu.user WHERE pu.party.id = :partyId")
     List<PartyUser> findUsersByPartyId(Long partyId);
+
+    boolean existsByUserIdAndPartyId(Long id, Long partyId);
+
+    boolean existsByPartyUserPK_UserIdAndPartyUserPK_PartyId(Long id, Long id1);
 }
