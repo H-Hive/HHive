37ec56057b98ec9ee19480095c7b479325f2b0f4
김은채 <ke808762@gmail.com>
2024-01-16 10:32:30 +0900

    feat: 로그인시 반환 정보 추가

--- src/main/java/com/HHive/hhive/domain/user/dto/UserInfoResponseDTO.java
+++ src/main/java/com/HHive/hhive/domain/user/dto/UserInfoResponseDTO.java
@@ -8,11 +8,13 @@ import lombok.Getter;
 @AllArgsConstructor
 public class UserInfoResponseDTO {
 
+    private Long userId;
     private String username;
     private String email;
     private String description;
 
     public UserInfoResponseDTO(User user) {
+        this.userId = user.getId();
         this.username = user.getUsername();
         this.email = user.getEmail();
         this.description = user.getDescription();
