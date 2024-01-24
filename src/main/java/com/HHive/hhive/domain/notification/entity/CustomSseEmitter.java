b988d0c55b5c34842dff1d4963e5c2b8b11a9d17
mixedtape <54096846+mixedtape@users.noreply.github.com>
2024-01-24 15:59:54 +0900

    feat: 알림 타임아웃 시간 설정

--- src/main/java/com/HHive/hhive/domain/notification/entity/CustomSseEmitter.java
+++ src/main/java/com/HHive/hhive/domain/notification/entity/CustomSseEmitter.java
@@ -7,9 +7,12 @@ import lombok.Setter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class CustomSseEmitter extends SseEmitter {
     private Long userId;
-
+    private static final Long DEFAULT_TIMEOUT = 300000L;
+    public CustomSseEmitter(Long userId) {
+        super(DEFAULT_TIMEOUT);
+        this.userId = userId;
+    }
 }
