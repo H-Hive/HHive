6e91d05123f13880b783fa8bf6f7eb202bd34fda
de123456sdf <cdaeun95@gmail.com>
2024-02-05 00:02:34 +0900

    유저가 생성한 파티 조회 service 추가

--- src/main/java/com/HHive/hhive/domain/party/repository/PartyRepository.java
+++ src/main/java/com/HHive/hhive/domain/party/repository/PartyRepository.java
@@ -2,7 +2,12 @@ package com.HHive.hhive.domain.party.repository;
 
 import com.HHive.hhive.domain.party.entity.Party;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PartyRepository extends JpaRepository<Party, Long> {
+
+    List<Party> findByUserIdAndIsDeletedIsFalse(Long userId);
 
 }
